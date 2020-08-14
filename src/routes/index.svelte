24e554bab499aa62e753b7d4924228bb8c366f40
Jayphen <software@jayphen.com>
2020-08-14 19:16:05 +0200

    Add a vb

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,26 @@
 <style>
-
+  div {
+    text-align: center;
+  }
+  img {
+    width: 20em;
+    max-width: 80%;
+  }
 </style>
 
 <svelte:head>
   <title>esky</title>
 </svelte:head>
 
-<h1>hello mate</h1>
+<div>
+  <h1>hello mate</h1>
 
-<p>I'll be with you in a minute</p>
+  <p>I'll be with you in a minute</p>
 
-<p>tooroo, esky</p>
+  <p>help yourself to a tinnie in the meantime</p>
+
+  <img src="/vb.jpg" alt="a badly drawn victoria bitter" />
+
+  <p>tooroo</p>
+
+</div>
