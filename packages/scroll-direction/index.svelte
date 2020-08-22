6977ad5c944652e33987004fcb7024e60c138087
Jayphen <software@jayphen.com>
2020-08-22 16:16:22 +0200

    Use velocity to determine direction

--- packages/scroll-direction/index.svelte
+++ packages/scroll-direction/index.svelte
@@ -1,45 +1,39 @@
 <script>
   export let touchOnly = false,
-    offset = 10;
+    minVelocity = 0.2;
 
   let touched = false;
 
   let direction = "initial";
-  let prevMove = 0;
+  let prevMove = [0, 0];
 
   function setStartPosition(e) {
     if (touchOnly) touched = true;
 
-    prevMove = e.touches[0].clientY;
+    prevMove = [e.touches[0].clientY, e.timeStamp];
   }
 
   function detectScrollDirection(e) {
     if (touchOnly && !touched) return;
 
-    let thisMove = window.scrollY;
-
+    const [prevY, prevTime] = prevMove;
+    const now = e.timeStamp;
     const { scrollHeight, clientHeight } = document.documentElement;
     const maxHeight = scrollHeight - clientHeight;
+    const elapsed = now - prevTime;
 
-    if (thisMove <= 0) {
-      direction = "up";
-      prevMove = thisMove;
-      return;
-    }
+    let thisY = window.scrollY;
+    if (thisY > maxHeight) thisY = maxHeight;
+    if (thisY <= 0) thisY = 0;
 
-    if (thisMove >= maxHeight) {
-      direction = "down";
-      prevMove = thisMove;
-      return;
-    }
+    const scrolled = thisY - prevY;
+    const velocity = scrolled / elapsed;
 
-    if (prevMove > thisMove + offset) {
-      direction = "up";
-    } else if (prevMove < thisMove - offset) {
-      direction = "down";
-    }
+    prevMove = [thisY, e.timeStamp];
 
-    prevMove = thisMove;
+    if (Math.abs(velocity) < minVelocity) return;
+
+    direction = velocity > 0 ? "down" : "up";
   }
 </script>
 
@@ -47,4 +41,4 @@
   on:touchstart|passive={setStartPosition}
   on:scroll|passive={detectScrollDirection} />
 
-<slot {direction} position={prevMove} />
+<slot {direction} position={prevMove[0]} />
