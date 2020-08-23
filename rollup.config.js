862ccece2f559f8b009f7e0e764e750f1f4029e8
Jayphen <software@jayphen.com>
2020-08-24 00:27:57 +0200

    Remove dist from git

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,22 @@ import globby from 'globby'
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 
-export default globby.sync('packages/**/index.js').map(inputFile => ({
-  input: inputFile,
-  output: {
-    file: inputFile.replace('index.js', 'dist/index.mjs'),
-    format: 'es'
-  },
-  output: {
-    file: inputFile.replace('index.js', 'dist/index.js'),
-    format: 'umd',
-    name: 'eskyTabs'
-  },
-  plugins: [
-    svelte(),
-    resolve()
-  ]
-}));
+export default globby.sync('packages/*/index.js').map(inputFile => {
+  return {
+    input: inputFile,
+    output: [{
+      file: inputFile.replace('index.js', 'dist/index.mjs'),
+        format: 'es'
+    },
+    {
+      file: inputFile.replace('index.js', 'dist/index.js'),
+        format: 'umd',
+        name: inputFile.replace(/.*\/(.*)\/.*/, "esky-$1")
+    }],
+    plugins: [
+      svelte(),
+      resolve()
+    ]
+  }
+});
 
